b6a68ee9cc1c5db4c5b57e5a1ce8aa44d8193607
straight-tamago <121408851+straight-tamago@users.noreply.github.com>
2022-12-26 09:09:51 +0900

    Add files via upload

---
+++ NoCameraSound/NoCameraSound/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NoCameraSound
+//
+//  Created by mini on 2022/12/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ NoCameraSound/NoCameraSound/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  NoCameraSound
+//
+//  Created by mini on 2022/12/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
