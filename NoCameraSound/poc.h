ba818ea259487d140b7edd37755c0843d05a8dfe
straight-tamago <121408851+straight-tamago@users.noreply.github.com>
2022-12-27 15:58:10 +0900

    4.2

---
+++ NoCameraSound/poc.h
@@ -0,0 +1,15 @@
+//
+//  Header.h
+//  No Camera Sound
+//
+//  Created by すとれーとたまご★ on 2022/12/26.
+//
+
+#ifndef poc_h
+#define poc_h
+
+#include <stdio.h>
+void startr_0(void);
+void startr_1(void);
+void startr_2(void);
+#endif /* Header_h */
