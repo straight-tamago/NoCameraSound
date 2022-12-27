393db7ada2a9a959a7ec1919addedde3dcc69dc2
straight-tamago <121408851+straight-tamago@users.noreply.github.com>
2022-12-27 15:58:10 +0900

    4.2

---
+++ NoCameraSound/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  NoCameraSound
+//
+//  Created by mini on 2022/12/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
